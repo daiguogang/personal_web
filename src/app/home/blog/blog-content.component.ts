8fa69bae85de9df2e893d4bb2073f40ff2628baa
AlphaIX <m.ydy@foxmail.com>
2018-04-11 09:26:18 +0800

    解决路由出口刷新问题

--- src/app/home/blog/blog-content.component.ts
+++ src/app/home/blog/blog-content.component.ts
@@ -1,18 +1,47 @@
 import {Component, OnInit} from "@angular/core";
 import {ActivatedRoute} from "@angular/router";
+import {CallService} from "../../common/service/call.service";
 
 @Component({
+  selector:'app-blog-content',
   templateUrl:'./blog-content.component.html',
   styleUrls:['../home.component.css']
 })
 
 export class BlogContentComponent implements OnInit {
 
-  constructor(private activeRoute:ActivatedRoute) {}
+  contentId:number;
+  contentTxt:string;
+  updateTime:any;
+  views:number;
+  like:number;
+
+
+  constructor(private activeRoute:ActivatedRoute,
+              private call:CallService) {}
 
   ngOnInit() {
     debugger;
-    let contentId = this.activeRoute.snapshot.params["contentId"];
+    // this.contentId = this.activeRoute.snapshot.params["contentId"];
+    this.activeRoute.params.subscribe(params => {
+      this.contentId = params.contentId;
+      this.getBlogContent();
+    });
+
+  }
+
+
+  getBlogContent() {
+    this.call.callService("/front/blogContent",
+      {
+        "contentId":this.contentId
+      },
+      (val) => {
+        this.contentTxt = val.data.contentTxt;
+        this.updateTime = val.data.updateTime;
+        this.views = val.data.views;
+        this.like = val.data.like;
+      });
   }
 
 }
--- src/app/home/blog/blog-list.component.ts
+++ src/app/home/blog/blog-list.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit,ChangeDetectorRef} from "@angular/core";
 import {BlogParamMessageService} from "../service/blog-param-message.service";
 import {ActivatedRoute, Router} from "@angular/router";
+import {CallService} from "../../common/service/call.service";
 
 @Component({
   templateUrl:'./blog-list.component.html',
@@ -9,24 +10,48 @@ import {ActivatedRoute, Router} from "@angular/router";
 })
 export class BlogListComponent implements OnInit {
 
-  testContent:string;
+  blogList:any;
   contentId:number;
   categoryId:number;
+  path:string;
+
 
   constructor(private paramMessage: BlogParamMessageService,
               private router:Router,
+              private call: CallService,
+              public changeDetectorRef:ChangeDetectorRef,
               private activeRoute:ActivatedRoute) {}
 
   ngOnInit() {
     debugger;
-    this.categoryId = this.activeRoute.snapshot.params["categoryId"];
-    // this.contentId = this.activeRoute.snapshot.params["contentId"];
+    // this.router.onSameUrlNavigation = "reload";
+    // this.changeDetectorRef.detectChanges();
+    // this.categoryId = this.activeRoute.snapshot.params["categoryId"];
+    this.activeRoute.params.subscribe(params => {this.categoryId = params.categoryId;});
     this.contentId = this.paramMessage.data ? this.paramMessage.data.contentId : undefined;
-    let path = "blog/list/"+this.categoryId+"/content";
+    this.path = "blog/list/"+this.categoryId+"/content";
     if(this.contentId !== undefined) {
-      this.router.navigate([path,this.contentId]);
+      this.router.navigate([this.path,this.contentId]);
     }
 
+    this.call.callService("/front/blogList",
+      {
+        "categoryId":this.categoryId,
+        "isBlog":1,
+        "isPublic":1
+      },
+      (val) => {
+      this.blogList = val.data;
+      });
+
+  }
+
+  onDetail(item) {
+    // this.changeDetectorRef.markForCheck();
+    debugger;
+
+    this.router.navigate([this.path,item.contentId]);
+    // this.router.onSameUrlNavigation="reload";
   }
 
 
